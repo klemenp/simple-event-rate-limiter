a866a12f8eab098b504eed7ad361fdc745fbbc6d
klemenp <klemen.polanec@gmail.com>
2016-12-13 17:01:34 +0100

    test case in progress

--- simple-event-rate-limiter-core/src/test/java/simpleeventratelimiter/BasicLimiterTest.java
+++ simple-event-rate-limiter-core/src/test/java/simpleeventratelimiter/BasicLimiterTest.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright © 2016 Klemen Polanec
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package simpleeventratelimiter;
 
 import junit.framework.TestCase;
@@ -15,7 +30,7 @@ public class BasicLimiterTest extends TestCase {
     public void testEventLimitException() throws Exception
     {
         Limiter limiter = BasicLimiter.getInstance();
-        for (int eventCnt = 0; eventCnt<20;eventCnt++)
+        for (int eventCnt = 1; eventCnt<=20;eventCnt++)
         {
             if (eventCnt>10)
             {
