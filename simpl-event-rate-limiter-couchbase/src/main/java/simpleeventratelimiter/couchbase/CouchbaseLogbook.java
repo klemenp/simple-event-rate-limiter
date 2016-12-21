11e57d1cb8ad813e7842acfa521c9e8d54a943b6
klemenp <klemen.polanec@gmail.com>
2016-12-21 16:40:27 +0100

    docker stuff

--- simpl-event-rate-limiter-couchbase/src/main/java/simpleeventratelimiter/couchbase/CouchbaseLogbook.java
+++ simpl-event-rate-limiter-couchbase/src/main/java/simpleeventratelimiter/couchbase/CouchbaseLogbook.java
@@ -4,4 +4,6 @@ package simpleeventratelimiter.couchbase;
  * Created by klemen on 20.12.2016.
  */
 public interface CouchbaseLogbook {
+    Long getShorttermCounter(String eventKey);
+    void increaseShorttermCounter(String eventKey);
 }
